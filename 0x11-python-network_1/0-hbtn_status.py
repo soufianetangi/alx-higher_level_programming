4dd639bc4530b59118985a0fac23a1ff77baf084
soufianetangi <soufianetangi2@gmail.com>
2024-07-05 07:47:54 -0700

    Project Python Network

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This script fetches https://alx-intranet.hbtn.io/status and displays
+the type, content, and utf8 decoded content of the response.
+"""
+
+import urllib.request
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode('utf-8')))
+
